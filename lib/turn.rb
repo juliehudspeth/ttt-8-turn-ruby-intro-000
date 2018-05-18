d1896098eade49abb7bd84b8d90fe82ef9e4243e
Julie <juliejhudspeth@gmail.com>
2018-05-18 16:02:17 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,7 +11,7 @@ def input_to_index(user_input)
 end
 
 def valid_move?(board,index)
-  if index.between(0,8) && position_taken?(board, index) == false
+  if index.between?(0,8) && position_taken?(board, index) == false
     true
   else
     false
